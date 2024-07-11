4f718feb50f87618b3a1faf1c5ada47a6059de70
Maryam Hussain <maryamghayyas2005@gmail.com>
2024-07-11 19:20:12 +0500

    ini the repo

---
+++ main.ts
@@ -0,0 +1,218 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+
+let enemies: string[] = ["Monster", "Dragon", "Zombie", "Snake"];
+let maxEnemyHealth: number = 100;
+let enemyAttackDamage: number = 25;
+let maxPlayerHealth: number = 100;
+let minPlayerHealth: number = 0;
+let playerAttackDamage: number = 30;
+let healthPotions: number = 3;
+let healingAmount: number = 30;
+let potionChance: number = 50;
+let enemiesDefeated: number = 0;
+
+let randomNumber = (max: number, min: number = 0): number => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+async function method() {
+  console.log("\t=======================================");
+  console.log("\t\tWelcome To The Dungeon!");
+  console.log("\t=======================================\n");
+
+  let player = await inquirer.prompt([
+    {
+      name: "player1",
+      type: "input",
+      message: "Please enter your name:"
+    }
+  ]);
+  let playerName = player.player1;
+
+  if (playerName) {
+    let start = await inquirer.prompt([
+      {
+        name: "confirmStart",
+        type: "confirm",
+        message: "Do you want to start the game?"
+      }
+    ]);
+
+    if (start.confirmStart) {
+      GAME: while (true) {
+        let enemyHealth: number = randomNumber(maxEnemyHealth);
+        let enemy: string = enemies[randomNumber(enemies.length - 1)];
+        console.log("\t=======================================");
+        console.log(`\t\tA ${enemy} has appeared!`);
+        console.log("\t=======================================\n");
+        console.log(`\tYour HP = ${maxPlayerHealth}`);
+        console.log(`\t${enemy}'s HP = ${enemyHealth}\n`);
+        while (enemyHealth > 0) {
+          
+
+          let userInput = await inquirer.prompt([
+            {
+              name: "input",
+              type: "list",
+              message: "\tWhat would you like to do:",
+              choices: ["\tHome", "\tAttack", "\tDrink Health Potion", "\tRun", "\tView Player's Profile","\tView Enemy's Profile", "\tExit"]
+            }
+          ]);
+
+          switch (userInput.input) {
+            case "\tHome":
+              let rename = await inquirer.prompt([
+                {
+                  name: "player1",
+                  type: "confirm",
+                  message: "Do You Want To Change The Player's name:"
+                }
+              ]);
+              //console.log(rename.player1)
+              if (rename.player1) {
+                player = await inquirer.prompt([
+                  {
+                    name: "player1",
+                    type: "input",
+                    message: "Please enter the player's name:"
+                  }
+                ]);
+                playerName = player.player1;
+              } else {
+                let homeStart = await inquirer.prompt([
+                  {
+                    name: "confirmStart",
+                    type: "confirm",
+                    message: "Do you want to start the game?"
+                  }
+                ]);
+                if (!homeStart.confirmStart) {
+                  console.log("\t=======================================")
+                  console.log("\t\t##THANKS FOR PLAYING!##")
+                  console.log("\t=======================================")
+                  console.log(`\tExiting the game...`);
+                  return;
+                }
+                break;
+              }
+              
+              
+            case "\tAttack":
+              let attackDamage: number = randomNumber(playerAttackDamage, 1);
+              let damageTaken: number = randomNumber(enemyAttackDamage, 1);
+              enemyHealth -= attackDamage;
+              maxPlayerHealth -= damageTaken;
+              console.log(`\n\tYou struck the ${enemy} with damage = ${attackDamage}`);
+              console.log(`\tYou received damage = ${damageTaken}  from the ${enemy}\n`);
+              if (maxPlayerHealth <= minPlayerHealth) {
+                console.log(`\n\tYou have taken too much damage, you are too weak to go on.`);
+                break GAME;
+              }
+              break;
+            case "\tDrink Health Potion":
+              if (healthPotions > 0) {
+                maxPlayerHealth += healingAmount;
+                healthPotions -= 1;
+                console.log(`\n\tYou drank the Health Potion, healing yourself for ${healingAmount} HP.
+\tYou now have ${maxPlayerHealth} HP.
+\tYou now have ${healthPotions} potions left.\n`);
+              } else {
+                console.log(`\n\tYou have no health potions left. Defeat enemies for a chance to get one.\n`);
+              }
+              break;
+            case "\tRun":
+              console.log(`\n\t!!!You ran away from the ${enemy}!!!\n`);
+            //   console.log(`\tYou ran away from the ${enemy}\n`);
+                            let control = await inquirer.prompt([
+                              {
+                                name: "control",
+                                type: "list",
+                                message: "What would you like to do:",
+                                choices: ["Continue the game", "Exit"]
+                              }
+                            ]);
+                    
+                            if (control.control === "Continue the game") {
+                              console.log("\n\t[You can continue your adventure!]");
+                              continue GAME;
+                            } else {
+                              console.log("\t=======================================")
+                              console.log("\t\t##THANKS FOR PLAYING!##")
+                              console.log("\t=======================================")
+                              console.log(`\n\tExiting the game...`);
+                              
+                              process.exit()
+                            }
+            case "\tView Player's Profile":
+              console.log(`\t=======================================`);
+              console.log(`\tName:${playerName}`);
+              console.log(`\tHP: ${maxPlayerHealth}`);
+              console.log(`\tHealth Potions: ${healthPotions}`);
+              console.log(`\tEnemies Defeated: ${enemiesDefeated}`);
+              console.log(`\t=======================================\n`);
+              break;
+            case "\tView Enemy's Profile":
+              console.log(`\t=======================================`);
+              console.log(`\tName:${enemy}`);
+              console.log(`\tHP: ${enemyHealth}`);
+              //console.log(`\tHealth Potions: ${healthPotions}`);
+              //console.log(`\tEnemies Defeated: ${enemiesDefeated}`);
+              console.log(`\t=======================================\n`);
+              break;
+            case "\tExit":
+              console.log("\t=======================================")
+              console.log("\t\t##THANKS FOR PLAYING!##")
+              console.log("\t=======================================")
+              console.log(`\tExiting the game...`);
+              return;
+          }
+        }
+
+        enemiesDefeated++;
+        console.log("\t=========================================");
+        console.log(`\t${enemy} has been defeated`);
+        console.log("\t=========================================");
+
+        console.log(`\n\t[You have ${maxPlayerHealth} HP left.]\n`);
+
+        if (randomNumber(100) < potionChance) {
+          healthPotions++;
+          console.log(`\n\t${enemy} dropped a health potion.`);
+          console.log(`\tYou now have ${healthPotions} health potions left.\n`);
+        }
+
+        let control = await inquirer.prompt([
+          {
+            name: "control",
+            type: "list",
+            message: "What would you like to do:",
+            choices: ["Continue the game", "Exit"]
+          }
+        ]);
+
+        if (control.control === "Continue the game") {
+          console.log("\n\t[You can continue your adventure!]\n");
+        } else {
+          
+          console.log("\t=======================================")
+          console.log(`\t\tExiting the game...`);
+          break;
+        }
+      }
+
+      if (maxPlayerHealth <= minPlayerHealth) {
+        console.log("\t====================================================")
+        console.log("\tYou limped out of the dungeon, weak from the battle.");
+        console.log("\t====================================================")
+
+      }
+    }
+  }
+  console.log("\t=======================================")
+  console.log("\t\t##THANKS FOR PLAYING!##")
+  console.log("\t=======================================")
+
+}
+
+method();
